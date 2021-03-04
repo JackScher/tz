ca8534ed0d013d1e4dc7e7c8bde63bc84282b777
JackScher <dzhek.layt298@gmail.com>
2021-03-04 21:46:21 +0300

    fix task2/task3

--- task2.py
+++ task2.py
@@ -8,17 +8,29 @@ class Replacement:
         'Str.': 'Street'
     }
 
-    def __init__(self, random_string):
-        self.arr = random_string.split()
+    exceptions_list = ['Ave Maria']
 
-    def checker(self):
-        for index in range(0, len(self.arr)):
-            if self.arr[index][-1] == ',':
-                self.arr[index] = self.service(self.arr[index][:-1])
-                self.arr[index] += ','
+    def __init__(self, random_string):
+        self.arr = []
+        self.exception = None
+        for exception in self.exceptions_list:
+            if exception in random_string:
+                self.exception = exception
+        if self.exception:
+            arr = random_string.split(self.exception)
+            for string in arr:
+                self.arr.append(string.split())
+        else:
+            self.arr = random_string.split()
+
+    def checker(self, list):
+        for index in range(0, len(list)):
+            if list[index][-1] == ',':
+                list[index] = self.service(list[index][:-1])
+                list[index] += ','
             else:
-                self.arr[index] = self.service(self.arr[index])
-        return self.arr
+                list[index] = self.service(list[index])
+        return list
 
     def service(self, item):
         for key in self.constants_dictionary:
@@ -26,12 +38,26 @@ class Replacement:
                 return self.constants_dictionary[key]
         return item
 
+    def helper(self):
+        result = ''
+        for item in self.arr:
+            result_arr = self.checker(item)
+            if result:
+                result += ' '.join(result_arr)
+            else:
+                result = ' '.join(result_arr)
+                result += ' ' + self.exception + ' '
+        return result
+
     def execute(self):
-        return ' '.join(self.checker())
+        for item in self.arr:
+            if type(item) == list:
+                return self.helper()
+        return ' '.join(self.checker(self.arr))
 
 
 if __name__ == '__main__':
     string = '555 Straight Stave Ave, San Francisco, CA 94104 ' \
-             '444 Ave Maria Stairway St., San Francisco, CA 94104 ' \
-             '9032 Flave Steep Str, San Francisco, CA 94104'
+             '9032 Flave Steep Str, San Francisco, CA 94104 ' \
+             '444 Ave Maria Stairway Street, San Francisco, CA 94104'
     print(Replacement(string).execute())
--- task3.py
+++ task3.py
@@ -2,27 +2,31 @@
  <4 === yellow\
  <5 === red"
 
-color_dictionary = {
-    'green': [3, 8, 13, 18, 23, 28, 33, 38, 43, 48, 53, 58],
-    'yellow': [4, 9, 14, 19, 24, 29, 34, 39, 44, 49, 54, 59],
-    'red': [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]
-}
+GREEN = 3
+YELLOW = 4
+RED = 5
 
+class Definer:
+    def __init__(self, number):
+        self.number = number
 
-def define_the_color(num):
-    if num < 0 or num > 60:
-        return 'Wrong num!'
-    for index in range(0, 12):
-        if num < color_dictionary['green'][index]:
+    def get_needed_data(self):
+        return self.number % 5
+
+    def define_the_color(self, number):
+        if number < GREEN:
             return 'green'
-        elif num >= color_dictionary['green'][index] and num < color_dictionary['yellow'][index]:
+        elif number >= GREEN and number < YELLOW:
             return 'yellow'
-        elif num >= color_dictionary['yellow'][index] and num < color_dictionary['red'][index]:
+        elif number >= YELLOW and number < RED:
             return 'red'
-        elif num == 60:
-            return 'red'
+
+    def execute(self):
+        if self.number >= 0:
+            return self.define_the_color(self.get_needed_data())
+        return 'Number less then 0.'
 
 
 if __name__ == '__main__':
-    num = float(input('Input any num between 0 and 60: '))
-    print(define_the_color(num))
+    num = float(input('Input any num: '))
+    print(Definer(num).execute())
